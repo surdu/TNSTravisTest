c2e12c5ba5fa482c1cfb12ced2ea1a07a1b37464
surdu <postal2600@gmail.com>
2018-02-19 11:25:56 +0200

    Switch to manually starting appium

--- wdio.conf.android.js
+++ wdio.conf.android.js
@@ -27,6 +27,7 @@ exports.config = merge(wdioConfBase.config, {
   appium: {
     args: {
       address: 'localhost',
+      port: '4723',
       commandTimeout: '7200',
       sessionOverride: true,
       debugLogSpacing: true,
@@ -36,11 +37,11 @@ exports.config = merge(wdioConfBase.config, {
       autoAcceptAlerts: true,
       app: APP_PATH
     }
-  },
-
-  onPrepare: function() {
-    let appiumController = require('appium-controller');
-    appiumController.startAppium();
   }
+
+  // onPrepare: function() {
+  //   let appiumController = require('appium-controller');
+  //   appiumController.startAppium();
+  // }
 
 });
