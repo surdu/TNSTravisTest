058304c64def63b2cedf039d8fad55cd7bfdf87c
surdu <nicu@surdu.me>
2018-06-11 11:49:37 +0300

    touch

--- .circleci/pushConfig.sh
+++ .circleci/pushConfig.sh
@@ -4,4 +4,3 @@ curl --user a8a763660d2d4ac201950725e6ed371c600d8aed: \
   --form config=@config.yml \
   --form notify=false \
     https://circleci.com/api/v1.1/project/github/surdu/TNSTravisTest/tree/master
-    
